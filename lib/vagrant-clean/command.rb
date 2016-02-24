7b7e648cc296403789c9422cbe14d53ca3adc873
Matt Spaulding <mspaulding06@gmail.com>
2016-02-24 15:20:21 -0800

    Finally got index to clean up

--- lib/vagrant-clean/command.rb
+++ lib/vagrant-clean/command.rb
@@ -11,11 +11,29 @@ module VagrantPlugins
                 end
 
                 argv = parse_options(opts)
+                prune = []
 
                 @env.machine_index.each do |entry|
-                    machine = @env.machine(entry.name, entry.provider)
-                    @env.ui.info("Destroying VM id #{machine.name}")
-                    machine.action(:destroy, force_confirm_destroy: true)
+                    env = entry.vagrant_env(entry.vagrantfile_path)
+                    machine = env.machine(entry.name.to_sym, entry.provider.to_sym)
+                    @env.ui.info("Destroying VM id #{entry.id[0..6]}")
+                    action_env = machine.action(:destroy, force_confirm_destroy: true)
+
+                    # Remove machine from index if it was destroyed
+                    if action_env.key?(:force_confirm_destroy_result) && action_env[:force_confirm_destroy_result] == true
+                        @env.ui.info("Destroyed")
+                    else
+                        @env.ui.warn("Failed to destroy VM id #{entry.id[0..6]}")
+                    end
+
+                    prune << entry
+                end
+
+                # Must remove machine from the global index
+                prune.each do |entry|
+                    e = @env.machine_index.get(entry.id)
+                    @env.machine_index.delete(e)
+                    @env.machine_index.release(e)
                 end
 
                 return 0
