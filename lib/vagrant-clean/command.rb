a532330657fdd03f5e8e851a8af99a3fcc326308
Matt Spaulding <mspaulding06@gmail.com>
2016-02-26 12:49:04 -0800

    Fixed home path bug and cleaned up output

--- lib/vagrant-clean/command.rb
+++ lib/vagrant-clean/command.rb
@@ -14,26 +14,31 @@ module VagrantPlugins
                 prune = []
 
                 @env.machine_index.each do |entry|
-                    env = entry.vagrant_env(entry.vagrantfile_path)
+                    begin
+                        env = entry.vagrant_env(@env.home_path)
+                    rescue Vagrant::Errors::EnvironmentNonExistentCWD
+                        entry = @env.machine_index.get(entry.id)
+                        @env.machine_index.delete(entry) if entry
+                        next
+                    end
                     machine = env.machine(entry.name.to_sym, entry.provider.to_sym)
-                    @env.ui.info("Destroying VM id #{entry.id[0..6]}")
+                    @env.ui.info("==> Destroying vm #{entry.name}:#{entry.id[0..6]}", bold: true)
                     action_env = machine.action(:destroy, force_confirm_destroy: true)
 
                     # Remove machine from index if it was destroyed
                     if action_env.key?(:force_confirm_destroy_result) && action_env[:force_confirm_destroy_result] == true
-                        @env.ui.info("Destroyed")
+                        @env.ui.success("==> Destroyed")
                     else
-                        @env.ui.warn("Failed to destroy VM id #{entry.id[0..6]}")
+                        @env.ui.warn("==> Warning: failed to destroy #{entry.name}:#{entry.id[0..6]}")
                     end
 
-                    prune << entry
+                    prune << entry.id
                 end
 
                 # Must remove machine from the global index
-                prune.each do |entry|
-                    e = @env.machine_index.get(entry.id)
-                    @env.machine_index.delete(e)
-                    @env.machine_index.release(e)
+                prune.each do |id|
+                    entry = @env.machine_index.get(id)
+                    @env.machine_index.delete(entry) if entry
                 end
 
                 return 0
